78d44c4ce8255554a5e4f4825a3a6e8c4ca6b1d2
Pastitata <mateus.j.andreo@gmail.com>
2025-11-24 13:31:04 -0300

    Update regfile.vhd

--- regfile.vhd
+++ regfile.vhd
@@ -1,12 +1,13 @@
 entity regfile is
   port(
-    clock        : in bit;
-    reset        : in bit;
-    regWrite     : in bit;
-    rr1          : in bit_vector(4 downto 0);
-    rr2          : in bit_vector(4 downto 0);
-    wr           : in bit_vector(4 downto 0);
-    d            : in bit_vector(63 downto 0);
-    q1           : out bit_vector(63 downto 0);
-    q2           : out bit_vector(63 downto 0);
-    
+    clock        : in bit;                      -- entrada de clock
+    reset        : in bit;                      -- entrada de reset
+    regWrite     : in bit;                      -- entrada de carga do registrador wr
+    rr1          : in bit_vector(4 downto 0);   -- entrada define registrador 1
+    rr2          : in bit_vector(4 downto 0);   -- entrada define registrador 2
+    wr           : in bit_vector(4 downto 0);   -- entrada define registrador de escrita
+    d            : in bit_vector(63 downto 0);  -- entrada de dado para carga pararela
+    q1           : out bit_vector(63 downto 0); -- saida do registrador rr1
+    q2           : out bit_vector(63 downto 0)  -- saida do registrador rr2
+  );
+end entity regfile;
