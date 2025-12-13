d88655d4ed8e2e9373037f4835346a27aee2ab19
GuiBaltar <126512092+GuiBaltar@users.noreply.github.com>
2025-12-13 12:05:57 -0300

    tb_polilegv8.vhd

--- tb_polilegv8.vhd
+++ tb_polilegv8.vhd
@@ -4,23 +4,21 @@ use ieee.numeric_bit.all;
 entity tb_polilegv8 is
 end entity;
 
-architecture sim of tb_polilegv8 is
+architecture tb_polilegv8Arch of tb_polilegv8 is
 
   signal clock : bit := '0';
   signal reset : bit := '1';
 
-  constant TCLK : time := 10 ns; -- 100 MHz (ajuste se quiser)
+  constant TCLK : time := 10 ns;
 
 begin
 
-  -- DUT
   dut : entity work.polilegv8
     port map(
       clock => clock,
       reset => reset
     );
 
-  -- Clock generator
   p_clk : process
   begin
     while true loop
@@ -31,19 +29,15 @@ begin
     end loop;
   end process;
 
-  -- Reset + tempo de simulação
   p_stim : process
   begin
-    -- reset assíncrono ativo no início
+    
     reset <= '1';
-    wait for 3*TCLK;      -- segura alguns ciclos
+    wait for 3*TCLK;    
 
-    reset <= '0';         -- libera
-    -- roda ciclos suficientes para executar o programa e entrar em loop
-    -- (ajuste se sua memória/programa tiver mais/menos instruções)
+    reset <= '0';        
     wait for 200*TCLK;
 
-    -- encerra simulação
     assert false report "Fim da simulacao (TB)" severity failure;
   end process;
 
